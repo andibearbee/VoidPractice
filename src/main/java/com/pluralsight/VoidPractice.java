864e4c21019fbce9d2225b37341e2554973d38c9
Andi <alexandra.e.burke@gmail.com>
2023-10-12 11:14:56 -0400

    VoidPractice Push

---
+++ src/main/java/com/pluralsight/VoidPractice.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class VoidPractice {
+    public static void main(String[] args) {
+        int addRes = add(2, 7);
+        System.out.println(addRes);
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
